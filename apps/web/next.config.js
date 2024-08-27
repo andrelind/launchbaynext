800047092cf192a98dac794baeac3679ecb62b9e
André Lind <andre@lindapp.se>
2024-08-27 14:37:41 +0200

    Fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-
+  experimental: { missingSuspenseWithCSRBailout: false, },
   env: {
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
