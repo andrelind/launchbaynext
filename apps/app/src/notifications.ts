2703d1b376a823fcd6778cf93d6d229bf4240596
André Lind <andre@lindapp.se>
2024-09-01 17:53:00 +0200

    Fix message

--- apps/app/src/notifications.ts
+++ apps/app/src/notifications.ts
@@ -4,13 +4,16 @@ export const notifications = [
     link: undefined,
     title: 'A New Hope',
     body: `The studio most known for nice models with some kind of dice game slapped on top of it finally let X-Wing go.
-The community is now in charge of the game's future, which is a great opportunity to make it awesome again!
+This is my personal view and does not reflect the opinion of the X-Wing Alliance in any way.
+
+The good news is that the community is now in charge of the game's future, which is a great opportunity to make it awesome again!
 
 To celebrate this new era, I've added the option to select rulesets when creating a new squadron, so you can play with 2.0 Legacy points, XWA points or whatever the community will come up with next.
 
 Also the app got a bit of new polish 😊
 Some features are still missing, but I'm working on it and plan on releasing them soon.
 
-Best Regards, André`,
+Best Regards, 
+André`,
   },
 ];
